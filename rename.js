52140270e54a81599c783812da7f3542c8ab8154
ankitch <ankitchhetri0@gmail.com>
2017-10-24 11:19:06 +0545

    sucessfully replacing words

--- rename.js
+++ rename.js
@@ -1,5 +1,21 @@
 let domelements = document.getElementsByTagName('*');
+for (var i = 0; i < domelements.length; i++){
+    let elements = domelements[i];
 
-for (let i in domelements.length){
-    let elements = domelements[i]
+    for (var j = 0; j <elements.childNodes.length; j++){
+        let node = elements.childNodes[j];
+        
+        if(node.nodeType === 3){
+            let text = node.nodeValue;
+
+            let replaceWithThis = text.replace(/Instagram/gi, "Snapchat Clone");
+    
+            if (replaceWithThis !== text){
+                elements.replaceChild(document.createTextNode(replaceWithThis),node);
+            }
+           
+        }
+    }
+        
+
 }
